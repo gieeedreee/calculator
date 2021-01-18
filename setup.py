0cceca559852464731b9aa7ceff225699eab4016
gieeedreee <66144591+gieeedreee@users.noreply.github.com>
2021-01-18 22:47:39 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='calculator215',
+    version='',
+    packages=['test', 'calculator'],
+    url='',
+    license='',
+    author='Giedre',
+    author_email='',
+    description=''
+)
